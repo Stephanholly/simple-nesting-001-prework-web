bab176c5dd176f3b7ea21aae0423c856f23acecc
Stephanholly <sholly@usc.edu>
2015-12-02 19:07:05 -0800

    fun

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,9 +53,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -77,6 +79,11 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+          },
+          :yukihiro_matsumoto => {
+            :known_for => "Ruby",
+            :languages => ["LISP", "C"]
+           
         }
      }
 
@@ -91,7 +98,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -100,7 +107,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     
      
      
 end
@@ -118,11 +125,10 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C", "Assembly"]
         }
      }
 
-     
 end
 
 
